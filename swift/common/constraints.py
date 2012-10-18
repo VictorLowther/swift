be6e5ddb0ba364ff3f2c35a4f355f56171664050
Victor Lowther <victor.lowther@gmail.com>
2012-10-18 11:12:16 -0500

    Fix pep8 violations.
    Change-Id: I7e532c74f8b456669e4cb2991f125422f63b765b

--- swift/common/constraints.py
+++ swift/common/constraints.py
@@ -13,7 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os,urllib
+import os
+import urllib
 from ConfigParser import ConfigParser, NoSectionError, NoOptionError, \
     RawConfigParser
 
