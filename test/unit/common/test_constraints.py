a0a32af381d2f0e88064bdff86e52ebecd8d45e4
Victor Lowther <victor.lowther@gmail.com>
2012-10-18 11:14:25 -0500

    Add a real UUID in test_constraints.py for testing of check_mount.
    Change-Id: Ifc15bc35ffef0b9c213f306b3c608ae81f7ef431

--- test/unit/common/test_constraints.py
+++ test/unit/common/test_constraints.py
@@ -174,6 +174,7 @@ class TestConstraints(unittest.TestCase):
         constraints.os = MockTrue()  # mock os module
         self.assertTrue(constraints.check_mount('/srv', '1'))
         self.assertTrue(constraints.check_mount('/srv', 'foo-bar'))
+        self.assertTrue(constraints.check_mount('/srv', '003ed03c-242a-4b2f-bee9-395f801d1699'))
         self.assertFalse(constraints.check_mount('/srv', 'foo bar'))
         self.assertFalse(constraints.check_mount('/srv', 'foo/bar'))
         self.assertFalse(constraints.check_mount('/srv', 'foo?bar'))
